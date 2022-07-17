cd58e1a1019e0e20db8cd1d418baa86e148752c1
pipimaho <mtateno0@gmail.com>
2022-07-17 10:37:52 +0900

    Entryモデルの作成

--- django_blogs/admin.py
+++ django_blogs/admin.py
@@ -2,6 +2,7 @@ from pydoc_data.topics import topics
 from tkinter import Toplevel
 from django.contrib import admin
 
-from .models import Topic
+from .models import Entry, Topic
 
 admin.site.register(Topic)
+admin.site.register(Entry)
--- django_blogs/models.py
+++ django_blogs/models.py
@@ -1,3 +1,5 @@
+from cgitb import text
+from re import T
 from statistics import mode
 from django.db import models
 
@@ -7,3 +9,14 @@ class Topic(models.Model):
 
   def __str__(self):
     return self.text
+
+class Entry(models.Model):
+  topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+  text = models.TextField()
+  date_added = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    verbose_name_plural = 'entries'
+
+    def __str__(self):
+      return f"{self.text[:25]}..."
