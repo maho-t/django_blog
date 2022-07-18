d4b48717ff8fa67968a7dd3b55dd2f5c87cca91e
pipimaho <mtateno0@gmail.com>
2022-07-18 10:15:52 +0900

    URLの対応づけ

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('django_blogs.urls')),
+]
+
+from django.urls import path
+from . import views
+
+app_name = 'django_blogs'
+urlpatterns = [
+    #ホームページ
+    path('', views.index, name='index'),
 ]
