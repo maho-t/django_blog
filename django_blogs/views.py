9856ff9c6ef23b07dd62e5a92cffec821bfba5b1
pipimaho <mtateno0@gmail.com>
2022-07-24 08:43:07 +0900

    新規トピック作成のためのビュー作成

--- django_blogs/views.py
+++ django_blogs/views.py
@@ -1,7 +1,9 @@
 from html import entities
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import redirect, render
 
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
   return render(request, 'django_blogs/index.html')
@@ -16,3 +18,15 @@ def topic(request, topic_id):
   entries = topic.entry_set.order_by('-date_added')
   context = {'topic': topic, 'entries': entries}
   return render(request, 'django_blogs/topic.html', context)
+
+def new_topic(request):
+  if request.method != 'POST':
+    form = TopicForm()
+  else:
+    form = TopicForm(data=request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('django_blogs:topics')
+
+  context = {'form': form}
+  return render(request, 'django_blogs/new_topic.html', context)
