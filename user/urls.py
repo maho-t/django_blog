35318c881eca998838014a2a9bcc2728ed441d13
pipimaho <mtateno0@gmail.com>
2022-07-27 10:41:27 +0900

    ユーザー登録機能の実装

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,12 @@
 
 from django.urls import URLPattern, path, include
 
+from . import views
+
 app_name = 'user'
 urlpatterns = [
+  # デフォルトの認証URLを読み込む
   path('', include('django.contrib.auth.urls')),
+  # ユーザー登録ページ
+  path('register/', views.register, name='register'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import imp
+from multiprocessing import context
+from django.shortcuts import redirect, render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import is_valid_path
 
-# Create your views here.
+def register(request):
+  if request.method != 'POST':
+    form = UserCreationForm()
+  else:
+    form = UserCreationForm(data=request.POST)
+    if form.is_valid():
+      new_user = form.save()
+      login(request, new_user)
+      return redirect('django_blogs:index')
+  
+  context = {'form': form}
+  return render(request, 'registration/register.html', context)
